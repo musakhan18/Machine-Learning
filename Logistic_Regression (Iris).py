ad8ceb9f057c9e5ee57fec4ca68fbf2d05ead004
Musa khan <musak1561@gmail.com>
2023-11-07 16:05:46 +0500

    Add files via upload

---
+++ Logistic_Regression (Iris).py
@@ -0,0 +1,54 @@
+# from sklearn import datasets
+# from sklearn.linear_model import LogisticRegression
+# import matplotlib.pyplot as plt
+# import numpy as np
+
+# iris=datasets.load_iris()
+
+# x=iris["data"][:,3:]
+# y=(iris["target"]==2).astype(int)
+# clf=LogisticRegression()
+# clf.fit(x,y)
+
+# example=clf.predict(([[2.6]]))
+# print(example)
+
+# x_new=np.linspace(0,3,1000).reshape(-1,1)
+# x_prob=clf.predict_log_proba(x_new)
+# plt.plot(x_new,x_prob[:,1],"g-",label="virginica")
+# plt.show()
+# Train a logistic regression classifier to predict whether a flower is iris virginica or not
+from sklearn import datasets
+from sklearn.linear_model import LogisticRegression
+import numpy as np
+import matplotlib.pyplot as plt
+iris = datasets.load_iris()
+# print(list(iris.keys()))
+# print(iris['data'].shape)
+# print(iris['target'])
+# print(iris['DESCR'])
+
+X = iris["data"][:, 3:]
+y = (iris["target"] == 2).astype(np.int)
+
+# Train a logistic regression classifier
+clf = LogisticRegression()
+clf.fit(X,y)
+example = clf.predict(([[2.6]]))
+print(example)
+
+# Using matplotlib to plot the visualization
+X_new = np.linspace(0,3,1000).reshape(-1,1)
+y_prob = clf.predict_proba(X_new)
+print(y_prob)
+plt.plot(X_new, y_prob[:,1], "g-", label="virginica")
+plt.show()
+
+
+# print(y)
+# print(iris["data"])
+# print(X)
+
+
+
+
